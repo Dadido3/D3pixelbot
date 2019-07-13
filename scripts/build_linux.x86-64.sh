7f4ec4aef2ed6f49b607fdbd68d3082c1d1479a1
David Vogel <Dadido3@aol.com>
2019-07-13 19:49:14 +0200

    Add build scripts & Deploy to releases

---
+++ scripts/build_linux.x86-64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir distribution
+
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+
+go build
+rice append --exec D3pixelbot
+
+wget https://github.com/c-smile/sciter-sdk/raw/master/bin.gtk/x64/libsciter-gtk.so
+
+7z a -t7z distribution/Linux.x86-64.7z -m0=lzma2 -mx=9 -aoa D3pixelbot README.md LICENSE config.json libsciter-gtk.so
+
+rm D3pixelbot
+rm libsciter-gtk.so
