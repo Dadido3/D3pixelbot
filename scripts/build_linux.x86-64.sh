3e88567d3a5bd39c6b5633d11356a0bf6acb1307
David Vogel <Dadido3@aol.com>
2019-07-13 20:53:24 +0200

    Another fix
    - Use batch file instead of power shell
    - Compress to tar.gz on linux

--- scripts/build_linux.x86-64.sh
+++ scripts/build_linux.x86-64.sh
@@ -2,6 +2,9 @@
 
 mkdir distribution
 
+sudo apt-get update
+sudo apt-get install -y libgtk-3-dev
+
 go get github.com/GeertJohan/go.rice
 go get github.com/GeertJohan/go.rice/rice
 
@@ -10,7 +13,8 @@ rice append --exec D3pixelbot
 
 wget https://github.com/c-smile/sciter-sdk/raw/master/bin.gtk/x64/libsciter-gtk.so
 
-7z a -t7z distribution/Linux.x86-64.7z -m0=lzma2 -mx=9 -aoa D3pixelbot README.md LICENSE config.json libsciter-gtk.so
+# 7z a -t7z distribution/Linux.x86-64.7z -m0=lzma2 -mx=9 -aoa D3pixelbot README.md LICENSE config.json libsciter-gtk.so
+env GZIP=-9 tar cfzv distribution/Linux.x86-64.tar.gz D3pixelbot README.md LICENSE config.json libsciter-gtk.so
 
 rm D3pixelbot
 rm libsciter-gtk.so
