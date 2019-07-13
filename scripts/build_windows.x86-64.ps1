b51e9e43a4beb3f845864c8a3c1087cfcb5e330e
David Vogel <Dadido3@aol.com>
2019-07-13 20:23:57 +0200

    Try to fix build scripts
    - chmod +x
    - Paths
    - use wget on windows

--- scripts/build_windows.x86-64.ps1
+++ scripts/build_windows.x86-64.ps1
@@ -1,14 +1,13 @@
-mkdir .\distribution
+mkdir distribution
 
 go get github.com/GeertJohan/go.rice
 go get github.com/GeertJohan/go.rice/rice
 
 go build
-rice append --exec .\D3pixelbot.exe
+rice append --exec D3pixelbot.exe
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Invoke-WebRequest -Uri https://github.com/c-smile/sciter-sdk/raw/master/bin/64/sciter.dll -OutFile .\sciter.dll
+wget https://github.com/c-smile/sciter-sdk/raw/master/bin/64/sciter.dll --output sciter.dll
 
-7z a -t7z .\distribution\Windows.x86-64.7z -m0=lzma2 -mx=9 -aoa D3pixelbot.exe README.md LICENSE config.json sciter.dll
+7z a -t7z distribution\Windows.x86-64.7z -m0=lzma2 -mx=9 -aoa D3pixelbot.exe README.md LICENSE config.json sciter.dll
 
-Remove-Item .\D3pixelbot.exe, .\sciter.dll
+Remove-Item D3pixelbot.exe, sciter.dll
